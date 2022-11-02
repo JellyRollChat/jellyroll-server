3c93c714e2b32da55453ae49eb3747f1de6476cd
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2022-11-02 16:23:38 -0500

    more more more status msgs?

--- main.go
+++ main.go
@@ -11,21 +11,27 @@ func main() {
 
 	createDirIfItDontExist("keys")
 
-	// serverKeys := initKeys()
-
 	fmt.Println("Server Pubkey:\t" + serverKeys.publicKey[0:4] + "..  ./" + pubKeyFilePath)
 	fmt.Println("Server Privkey:\t" + serverKeys.privateKey[0:4] + "..  ./" + privKeyFilePath)
 	fmt.Println("Server Sigkey:\t" + serverKeys.signedKey[0:4] + "..  ./" + signedKeyFilePath)
 	fmt.Println("Server Cert:\t" + serverKeys.selfCert[0:4] + "..  ./" + selfCertFilePath)
 
-	announce("Socket API up!")
-
+	// open tracker channel
+	announce("Tracker Socket Up!")
 	go TrackerSocketAPI(serverKeys)
-
 	fmt.Println("Tracker Port:\t" + strconv.Itoa(trackerCommPort))
+
+	// open server channel
+	announce("Server Socket Up!")
+	go TrackerSocketAPI(serverKeys)
 	fmt.Println("Server Port:\t" + strconv.Itoa(serverCommPort))
+
+	// open client channel
+	announce("Client Socket Up!")
+	go TrackerSocketAPI(serverKeys)
 	fmt.Println("Client Port:\t" + strconv.Itoa(clientCommPort))
 
+	// blocking operation
 	select {}
 }
 
