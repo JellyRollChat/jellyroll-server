c1de822eabfecd13b2874f0a4d7336f93b8a86b4
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2022-11-02 18:57:25 -0500

    everyone gets a parser
    message parser needed to be split up

--- api-client.go
+++ api-client.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
+	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 func ClientSocketAPI(keyCollection *ED25519Keys) {
@@ -19,9 +23,66 @@ func ClientSocketAPI(keyCollection *ED25519Keys) {
 		conn, _ := upgrader.Upgrade(w, r, nil)
 		defer conn.Close()
 		fmt.Printf(brightgreen+"\n[%s] [%s] +client\n"+white, timeStamp(), conn.RemoteAddr())
-		socketAuthAgent(conn, keyCollection)
+		socketTrackerParser(conn, keyCollection)
 	})
 
 	// Serve via HTTP
 	http.ListenAndServe(":"+strconv.Itoa(clientCommPort), handlers.CORS(headersCORS, originsCORS, methodsCORS)(api))
+}
+
+func socketClientParser(conn *websocket.Conn, keyCollection *ED25519Keys) {
+
+	for {
+
+		defer conn.Close()
+
+		msgType, msg, err := conn.ReadMessage()
+
+		if err != nil {
+
+			// socket session closed
+			fmt.Printf(brightyellow+"\n[%s] [%s] Peer disconnected\n"+white, timeStamp(), conn.RemoteAddr())
+
+			return
+		}
+
+		// if the socket has a join message
+		if bytes.HasPrefix(msg, joinMsg) {
+
+			msgToString := string(msg)
+
+			trimNewlinePrefix := strings.TrimRight(msgToString, "\n")
+
+			trimmedPubKey := strings.TrimLeft(trimNewlinePrefix, "JOIN ")
+
+			// complains about loop duration
+			regValidate, _ := regexp.MatchString(`[a-f0-9]{64}`, trimmedPubKey[:64])
+			if !regValidate {
+				fmt.Printf("\nContains illegal characters")
+				conn.Close()
+				return
+			}
+
+		}
+		if bytes.HasPrefix(msg, rtrnMsg) {
+			fmt.Printf("\nreturn message: %s", string(msg))
+
+			input := strings.TrimLeft(string(msg), "RTRN ")
+			var cert = strings.Split(input, " ")
+
+			if !verifySignature(cert[0], keyCollection.publicKey, cert[1]) {
+				fmt.Printf("\nsig doesnt verify")
+			}
+			if verifySignature(cert[0], keyCollection.publicKey, cert[1]) {
+				fmt.Printf("\nsig verifies")
+			}
+		}
+		if bytes.HasPrefix(msg, pubkMsg) {
+			conn.WriteMessage(msgType, []byte(keyCollection.publicKey))
+		}
+		if bytes.HasPrefix(msg, peerMsg) {
+			conn.WriteMessage(msgType, []byte(keyCollection.publicKey))
+		}
+
+	}
 }
