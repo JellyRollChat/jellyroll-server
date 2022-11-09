972b3411fde367011d99fb00244c957874217979
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2022-11-08 19:23:01 -0600

    adding signup interface

--- api-http.go
+++ api-http.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -12,7 +13,10 @@ import (
 func serverWebAPI() {
 
 	api := mux.NewRouter()
-	// api.HandleFunc("/keys/new", SignupHandler).Methods(http.MethodGet)
+	api.HandleFunc("/signup", SignupHandlerGET).Methods(http.MethodGet)
+	api.HandleFunc("/signup/", SignupHandlerGET).Methods(http.MethodGet)
+	api.HandleFunc("/signup", SignupHandlerGET).Methods(http.MethodGet)
+	api.HandleFunc("/signup/", SignupHandlerGET).Methods(http.MethodGet)
 
 	// Serve via HTTP
 	http.ListenAndServe(":"+strconv.Itoa(webPort), handlers.CORS(headersCORS, originsCORS, methodsCORS)(api))
@@ -21,4 +25,32 @@ func serverWebAPI() {
 func reportRequest(name string, w http.ResponseWriter, r *http.Request) {
 	userAgent := r.UserAgent()
 	fmt.Printf(brightgreen+"\n/%s"+white+" by "+brightcyan+"%s\n"+white+"Agent: "+brightcyan+"%s\n"+nc, name, r.RemoteAddr, userAgent)
+}
+
+func SignupHandlerGET(w http.ResponseWriter, r *http.Request) {
+
+	files := []string{
+		"templates/signup.html",
+	}
+
+	// Parse the file list
+	t, parseSignupFiles := template.ParseFiles(files...)
+
+	// if something goes wrong, report it, and where
+	// the error was generated.
+	handle("", parseSignupFiles)
+	if parseSignupFiles != nil {
+
+		// if something went wrong, browsers should be relayed a
+		// an Internal Server Error status.
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+
+		// stop execution
+		return
+	}
+
+	// if we did everything right, this should serve the request.
+	whatswrong := t.Execute(w, r)
+	handle("http signup render error", whatswrong)
+
 }
