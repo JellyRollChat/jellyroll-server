2a00f33edf137ca82e9293dd16cee8538fb10da8
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2022-12-18 10:28:35 -0600

    new signup flow

--- api-web.go
+++ api-web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -14,8 +15,6 @@ import (
 func serverWebAPI() {
 
 	api := mux.NewRouter()
-	// api.HandleFunc("/signup", SignupHandlerGET).Methods(http.MethodGet)
-	// api.HandleFunc("/signup/", SignupHandlerGET).Methods(http.MethodGet)
 	api.HandleFunc("/signup", SignupHandlerPOST).Methods(http.MethodPost)
 	api.HandleFunc("/signup/", SignupHandlerPOST).Methods(http.MethodPost)
 
@@ -27,76 +26,61 @@ func reportRequest(name string, w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("\n"+purple+r.Method+" "+brightgreen+"/%s"+white+" by "+brightcyan+"%s\n"+white+"Agent: "+cyan+"%s\n"+nc, name, r.RemoteAddr, userAgent)
 }
 
-// func SignupHandlerGET(w http.ResponseWriter, r *http.Request) {
-// 	reportRequest("signup", w, r)
-// 	files := []string{
-// 		"templates/signup.html",
-// 	}
-
-// 	t, parseSignupFiles := template.ParseFiles(files...)
-
-// 	handle("", parseSignupFiles)
-// 	if parseSignupFiles != nil {
-
-// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-
-// 		return
-// 	}
-
-// 	whatswrong := t.Execute(w, servertld)
-// 	handle("http signup render error", whatswrong)
-
-// }
-
 func SignupHandlerPOST(w http.ResponseWriter, r *http.Request) {
 	reportRequest("signup", w, r)
 
-	type signupObject struct {
-		username   string
-		password   string
-		passRepeat string
+	type SignupObject struct {
+		Username string
+		Password string
 	}
 
-	thisSignup := signupObject{}
+	thisSignup := SignupObject{}
+
+	// unmarshall json string to struct
+	err := json.NewDecoder(r.Body).Decode(&thisSignup)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	r.ParseForm()
 	for key, value := range r.Form {
 		if key == "signupUsername" {
-			thisSignup.username = sanitizeString(value[0], 20)
+			thisSignup.Username = sanitizeString(value[0], 20)
 		} else if key == "signupPassword" {
-			thisSignup.password = value[0]
+			thisSignup.Password = value[0]
 		}
 	}
 
-	if !fileContainsString(thisSignup.username, "admin/users.list") {
+	if !fileContainsString(thisSignup.Username, "admin/users.list") {
 
-		if len(thisSignup.username) < 6 {
+		if len(thisSignup.Username) < 6 {
 			log.Println(brightred + "Username is too short " + nc)
 			fmt.Fprintf(w, "Username is too short ")
 			return
 		}
 
-		if len(thisSignup.username) > 20 {
+		if len(thisSignup.Username) > 20 {
 			log.Println(brightred + "Username is too long " + nc)
 			fmt.Fprintf(w, "Username is too long ")
 			return
 		}
 
-		if len(thisSignup.password) < 8 {
+		if len(thisSignup.Password) < 8 {
 			log.Println(brightred + "password is too short " + nc)
 			fmt.Fprintf(w, "Password is too short ")
 			return
 		}
 
-		if len(thisSignup.password) > 120 {
+		if len(thisSignup.Password) > 120 {
 			log.Println(brightred + "password is too long " + nc)
 			fmt.Fprintf(w, "Password is too long ")
 			return
 		}
 
-		thisSignup.password = hashit(thisSignup.password)
+		thisSignup.Password = hashit(thisSignup.Password)
 
-		appendFile("admin/users.list", thisSignup.username+"::"+thisSignup.password+"\n")
+		appendFile("admin/users.list", thisSignup.Username+"::"+thisSignup.Password+"\n")
 
 		files := []string{
 			"templates/signupSuccess.html",
@@ -120,14 +104,14 @@ func SignupHandlerPOST(w http.ResponseWriter, r *http.Request) {
 		thisUser := userInfo{}
 
 		thisUser.Servertld = servertld
-		thisUser.Username = thisSignup.username
+		thisUser.Username = thisSignup.Username
 
 		whatswrong := t.Execute(w, thisUser)
 		handle("http signup success render error", whatswrong)
 
 		fmt.Fprintf(w, "\"OK\"")
 
-		log.Println(brightmagenta + "New User: " + magenta + thisSignup.username + "@" + servertld)
+		log.Println(brightmagenta + "New User: " + magenta + thisSignup.Username + "@" + servertld)
 		// return
 
 	} else {
