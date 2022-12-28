1d9c13cf4f1aaf9b7e549ed50ecf1c619b6cdbb2
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2022-12-28 01:02:11 -0600

    make it so

--- api-web.go
+++ api-web.go
@@ -23,6 +23,7 @@ func WebAPI() {
 
 	corsOrigins := []string{
 		"http://127.0.0.1:1430",
+		"http://server.3ck0.com:5270",
 	}
 
 	corsMethods := []string{
@@ -61,8 +62,7 @@ func WebAPI() {
 // }
 
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:1430")
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://server.3ck0.com:5270")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	(*w).Header().Set("Content-Type", "application/json")
