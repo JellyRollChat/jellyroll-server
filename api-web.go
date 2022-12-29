f88e055c7026f4b21ffd6f915e5f4966a43a3e05
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2022-12-29 14:48:44 -0600

    setting access-control-allow-credentials to true

--- api-web.go
+++ api-web.go
@@ -60,6 +60,7 @@ func WebAPI() {
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:1430")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	(*w).Header().Set("Content-Type", "application/json")
 }
